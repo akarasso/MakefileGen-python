d3a5a99ebcefc519bdda0acb01c0d616df6bee45
Alexandre KARASSOULOFF <akarasso@e2r3p13.42.fr>
2018-04-03 14:37:06 +0200

    fix...

--- genmake.py
+++ genmake.py
@@ -17,10 +17,10 @@ pro_type = ""
 pro_name = ""
 
 while not(pro_type == "prog" or pro_type == "lib"):
-	pro_type = input("Type de projet:(PROG/lib)")
+	pro_type = raw_input("Type de projet:(PROG/lib)")
 	pro_type = pro_type or "prog"
 while pro_name == "":
-	pro_name = input("Nom du projet: ")
+	pro_name = raw_input("Nom du projet: ")
 	pro_name = pro_name or ""
 
 if pro_type == "prog":
@@ -43,10 +43,13 @@ write_var(mf, "NAME", pro_name)
 if len(list_h) > 0:
 	write_var(mf, "HEADERS", list_h)
 	write_var(mf, "HEADERFLAGS", "$(addprefix -I, $(dir $(HEADERS)))")
+else:
+    write_var(mf, "HEADERS", "./includes")
 if pro_type == "prog":
 	write_var(mf, "LIBS", tr_str(list_libs))
 else:
-	if pro_name in list_libs:
+    if len(list_libs) > 0:
+        if pro_name in list_libs:
 		list_libs.remove(pro_name)
 	write_var(mf, "LIBS", tr_str(list_libs))
 write_var(mf, "SOURCES", list_src)
@@ -55,7 +58,7 @@ write_rule(mf, "all", "$(NAME)")
 if len(list_h) > 0:
 	write_rule(mf, "%.o", "%.c $(HEADERS)", ["$(CC) $(CFLAGS) $(HEADERFLAGS) -c -o $@ $<"])
 else:
-	write_rule(mf, "%.o", "%.c $(HEADERS)", ["$(CC) $(CFLAGS) -c -o $@ $<"])
+	write_rule(mf, "%.o", "%.c $(HEADERS)", ["$(CC) $(CFLAGS) -I$(HEADERS) -c -o $@ $<"])
 if pro_type == "lib":
 	write_rule(mf, "$(NAME)", "$(OBJ)", ["ar rcs $(NAME) $(OBJ)"])
 elif pro_type == "prog":
